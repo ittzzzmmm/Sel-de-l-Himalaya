dcb758f137711cd56bd06ba49db09ce8c85f6aa9
ittzzzmmm <ittzzzmmm@gmail.com>
2025-02-01 15:15:03 +0700

    ittzm

--- Itt_menu_receipt.cpp
+++ Itt_menu_receipt.cpp
@@ -6,29 +6,29 @@
 #include<vector>
 using namespace std;
 
-void import_price(vector<int> &menu_tag){
+void import_price(vector<int> &Itt_menu_tag){
     ifstream list("menu_tag.txt");
     string temp;
     while(getline(list,temp)){
-        menu_tag.push_back(stof(temp));}
+        Itt_menu_tag.push_back(stof(temp));}
     list.close();
 }
 
 
-void import_menu(vector<string> &menu_name){
+void import_menu(vector<string> &Itt_menu_name){
     ifstream list("menu_original.txt");
     string temp;
     while(getline(list,temp)){
-        menu_name.push_back(temp);}
+        Itt_menu_name.push_back(temp);}
     list.close();
 }
 
 int main(){
 
-    vector<int> menu_tag;
-    vector<string> menu_name;
-    vector<float> menu_price;
+    vector<int> Itt_menu_tag;
+    vector<string> Itt_menu_name;
+    vector<float> Itt_menu_price;
     
-    import_menu(menu_name); // เราได้ array ของ menu_name เเล้ว
-    cout << menu_name[0]<<" "<< menu_name[1]<<" "<< menu_name[2];    
+    import_menu(Itt_menu_name); // เราได้ array ของ menu_name เเล้ว
+    cout << Itt_menu_name[0]<<" "<< Itt_menu_name[1]<<" "<< Itt_menu_name[2];    
 }
